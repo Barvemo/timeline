4846dbd44274222e51781d4360b5c4e926c64649
Linus <linus.barvemo@gmail.com>
2026-03-29 15:48:39 +0200

    remove unnecessary extra code defining timestamp variable on own row

--- utils/build_output.py
+++ utils/build_output.py
@@ -2,10 +2,8 @@ import os
 from datetime import datetime
 
 def build_output(input_path, output_arg=None):
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')
-
     base_path = output_arg or input_path
-    output_folder = os.path.join(base_path, f"timeline_{timestamp}")
+    output_folder = os.path.join(base_path, f"timeline_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')}")
 
     os.makedirs(output_folder, exist_ok=True)
 
