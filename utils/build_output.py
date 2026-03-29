b8a6766ac33b92cfea525c069a71c59385ba9a0b
Linus <linus.barvemo@gmail.com>
2026-03-29 15:47:03 +0200

    cleanup output folder determination logic

--- utils/build_output.py
+++ utils/build_output.py
@@ -2,10 +2,10 @@ import os
 from datetime import datetime
 
 def build_output(input_path, output_arg=None):
-    if output_arg:
-        output_folder = os.path.join(output_arg, f"timeline_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')}")
-    else:
-        output_folder = os.path.join(input_path, f"timeline_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')}")
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')
+
+    base_path = output_arg or input_path
+    output_folder = os.path.join(base_path, f"timeline_{timestamp}")
 
     os.makedirs(output_folder, exist_ok=True)
 
